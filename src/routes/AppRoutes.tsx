b14386d51fc6a4b011b5cfcf9f9a191a29f150bd
Vigneshwari3108 <vigneshwari.yoho@gmail.com>
2025-06-30 17:14:53 +0530

    changes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,4 @@
 import AboutCourse from '@/components/courses/AboutCourse';
-import Notes_Materials from '@/components/courses/Notes_Materials';
 import { useAuth } from '@/context/AuthContext/AuthContext';
 import MainLayout from '@/layout/MainLayout';
 import ActivityLogs from '@/pages/ActivityLogs/ActivityLogs';
@@ -24,9 +23,10 @@ import Placement from '@/pages/Placement/Placement';
 import Profile from '@/pages/Profile/Profile';
 import TicketId from '@/pages/TicketId/TicketId';
 import Tickets from '@/pages/Tickets/Tickets';
-import Taskprojects from '@/components/courses/Task_projects';
+
 
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Batches from '@/components/courses/Batches';
 
 const AppRoutes = () => {
 	// const { isAuthenticated } = useAuth();
@@ -64,9 +64,8 @@ const AppRoutes = () => {
 				<Route path='payment' element={<Payment />} />
 				<Route path='about' element={<AboutCourse />} />
 				<Route path='note_materials' element={<NotesMaterials />} />
-				<Route path='task_projects' element={<Taskprojects />} />
 				<Route path='/about/:course' element={<AboutCourse />} />
-				<Route path='notes_materials' element={<Notes_Materials />} />
+				<Route path='batches' element={<Batches />} />
 			</Route>
 		</Routes>
 	);
