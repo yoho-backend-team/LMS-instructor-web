ea51712e176bc655cee8fba5cdb919ea842d0de9
suryas-1805 <suryas18051998@gmail.com>
2025-07-25 09:49:53 +0530

    pull from suruthiga

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import DashBoardSlice from '../features/Dashboard/reducers/slices';
 import TicketSlice from '../features/Tickets/reducer/TicketSlice';
 import ClassSlice from '../features/classes/reducers/slices';
+import ActivityLogsSlice from '../features/activitylog/reduces/ActivitySlice';
+import AttendanceSlice from '../features/attentance/reduces/AttendanceSlice';
 
 const store = configureStore({
 	reducer: {
 		dashboard: DashBoardSlice,
 		TicketSlice: TicketSlice,
 		ClassSlice: ClassSlice,
+		ActivityLogsSlice: ActivityLogsSlice,
+		AttendanceSlice: AttendanceSlice,
 	},
 });
 
